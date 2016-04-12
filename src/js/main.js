228e6429a8f0185ce0e8651f6397c4c40fe411b9
Haroen Viaene <hello@haroen.me>
2016-04-12 11:40:32 +0200

    toggle mute video on click

--- src/js/main.js
+++ src/js/main.js
@@ -208,7 +208,7 @@ var step = {
       }, 1000);
       // video
       if (typeof this.data[this.number].video !== 'undefined') {
-        video.innerHTML = '<video src="src/vid/' + this.data[this.number].video + '" autoplay ><p>oops! no video 😢</p></video>';
+        video.innerHTML = '<video src="src/vid/' + this.data[this.number].video + '" autoplay onclick="javascript:this.muted = !this.muted;"><p>oops! no video 😢</p></video>';
       } else {
         video.innerHTML = '<div id="video" class="video"><div class="video--intro"><p>JWR Mission Control</p></div></div>';
       }
