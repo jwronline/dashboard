cc16d0697dcd592754f17427e3bb9bb83e1e2690
Haroen Viaene <hello@haroen.me>
2016-10-30 19:11:11 +0100

    style fixes

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 
   // external urls should be opened in the regular browser
   function handleRedirect(e, url) {
-    if(url.includes('http')) {
+    if (url.includes('http')) {
       e.preventDefault();
       opn(url);
     }
@@ -38,7 +38,7 @@ function createWindow() {
   mainWindow.webContents.on('new-window', handleRedirect);
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -51,7 +51,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -59,7 +59,7 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
